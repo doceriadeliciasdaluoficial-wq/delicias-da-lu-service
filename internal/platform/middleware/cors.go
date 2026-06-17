4ab39b0f88316286cc4be16d232fc6e6e4131100
juliosshoji <zotokoba@gmail.com>
2026-06-17 17:32:35 -0300

    fix: adjusting CORS and swagger

--- internal/platform/middleware/cors.go
+++ internal/platform/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 func CORSMiddleware() echo.MiddlewareFunc {
 	return middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://deliciasdaluoficial.com.br", "https://delicias-da-lu.com", "https://www.delicias-da-lu.com", "https://delicias-da-lu-frontend-514609008596.southamerica-east1.run.app"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://deliciasdaluoficial.com.br", "https://delicias-da-lu.com", "https://www.delicias-da-lu.com", "https://delicias-da-lu-frontend-514609008596.us-east1.run.app"},
 		AllowMethods:     []string{"GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-Requested-With"},
 		ExposeHeaders:    []string{"Link"},
